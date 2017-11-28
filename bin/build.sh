c2f4fb623e0811507baefd3f199da5bebf3bef80
Brad Jewell <brad811@gmail.com>
2017-11-28 17:28:07 -0600

    add commands for rspec to build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+apt-get update
+apt-get install -y nodejs
+
+bundle install
+bundle exec rspec
+
 gem install fury
 gem build enc_dashboard.gemspec
 fury push --as=moovweb --api-token=$BUNDLE_GEM__FURY__IO *.gem
