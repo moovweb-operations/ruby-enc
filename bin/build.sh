75b6f0e7abad068e62a9c23952dc56ce52cc37d0
Brad Jewell <brad811@gmail.com>
2017-12-01 13:55:14 -0600

    make requested changes

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-#set -e
-#set -x
-
-#apt-get update
-#export DEBIAN_FRONTEND=noninteractive
-#apt-get install -y nodejs mysql-server
-#service mysql start
-
-#bundle install
-#bundle exec rake db:create RAILS_ENV=test
-#bundle exec rake db:migrate RAILS_ENV=test
-#bundle exec rspec
+set -e
+set -x
 
 gem install fury
 gem build enc_dashboard.gemspec
